d821132346baf55bff1a893a87beee4f2969ccc0
Jinnah Ali-Clarke <ja@Jinnahs-MacBook-Pro.local>
2019-09-05 13:34:42 -0400

    refactor

--- lib/src/SocketContextInterpreter.hs
+++ lib/src/SocketContextInterpreter.hs
@@ -8,6 +8,8 @@ module SocketContextInterpreter (
     withSocketContext
 ) where
 
+import qualified Data.ByteString.Char8 as B
+
 import Effects.SocketContext
 import Redis
 
@@ -15,7 +17,10 @@ newtype SocketContextInterpreter m a = SocketContextInterpreter (m a)
     deriving (Functor, Applicative, Monad)
 
 instance SocketContext m => Redis (SocketContextInterpreter m) where
-    get = const $ SocketContextInterpreter $ mempty <$ writeCommunicator "*2\r\n$3\r\nGET\r\n$10\r\nthis_thing\r\n"
+    get key =
+        let keyLength = B.length key
+            stringToSend = "*2\r\n$3\r\nGET\r\n$" <> B.pack (show keyLength) <> "\r\n" <> key <> "\r\n"
+        in SocketContextInterpreter $ mempty <$ writeCommunicator stringToSend
         
     set = undefined
 
--- lib/unit/Spec.hs
+++ lib/unit/Spec.hs
@@ -1,15 +1,29 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE
+    OverloadedStrings,
+    RankNTypes
+#-}
 
 import Test.Hspec
 import MockSocketContext
 
-import SocketContextInterpreter
+import qualified Data.ByteString as B
+
+import Effects.SocketContext
 import Redis
+import SocketContextInterpreter
+
+buffersFromSocketContext :: (forall m. Redis m => m a) -> SocketBuffers -> Either SocketContextError (a, SocketBuffers)
+buffersFromSocketContext action = runMockSocketContext (withSocketContext action)
+
+txBufferFromSocketContext :: (forall m. Redis m => m a) -> SocketBuffers -> Either SocketContextError B.ByteString
+txBufferFromSocketContext action = fmap (\(_, (SocketBuffers _ tx)) -> tx) . buffersFromSocketContext action
 
 main :: IO ()
 main = hspec $ do
     describe "withSocketContext" $ do
         it "should write bytes according to redis protocol to socket when doing a get" $ do
-            let result = runMockSocketContext (withSocketContext (get "this_thing")) (SocketBuffers mempty mempty)
-                tx = fmap (\(_, (SocketBuffers _ tx')) -> tx') result
-            tx `shouldBe` Right "*2\r\n$3\r\nGET\r\n$10\r\nthis_thing\r\n"
+            let result = txBufferFromSocketContext (get "this_thing") (SocketBuffers mempty mempty)
+            result `shouldBe` Right "*2\r\n$3\r\nGET\r\n$10\r\nthis_thing\r\n"
+        it "should write bytes according to redis protocol to socket when doing a get different key" $ do
+            let result = txBufferFromSocketContext (get "that_boi") (SocketBuffers mempty mempty)
+            result `shouldBe` Right "*2\r\n$3\r\nGET\r\n$8\r\nthat_boi\r\n"
