a3e4c8b55b9d1fd500cace43f1f116d2b62b72ad
Jinnah Ali-Clarke <ja@Jinnahs-MacBook-Pro.local>
2019-09-06 10:53:15 -0400

    refactor

--- lib/src/Interpreters/SocketContextInterpreter.hs
+++ lib/src/Interpreters/SocketContextInterpreter.hs
@@ -10,11 +10,12 @@ module Interpreters.SocketContextInterpreter (
 
 import Data.Functor (void)
 
-import Data.ByteString.Lazy.Char8 (pack, unpack)
+import Data.ByteString.Lazy.Char8 (unpack)
 import qualified Data.ByteString.Lazy as B
 import qualified Data.ByteString.Builder as B
 
 import Effects.SocketContext
+import Interpreters.RequestInterpreter
 import Redis
 
 newtype SocketContextInterpreter m a = SocketContextInterpreter (m a)
@@ -31,23 +32,15 @@ readUntil :: SocketContext m => (B.ByteString -> Bool) -> m B.ByteString
 readUntil predicate = fmap B.toLazyByteString (readUntil' predicate mempty)
 
 instance SocketContext m => Redis (SocketContextInterpreter m) where
-    get key =
-        let keyLength = B.length key
-            stringToSend =
-                B.lazyByteString "*2\r\n$3\r\nGET\r\n$"
-                <> B.lazyByteString (pack $ show keyLength)
-                <> B.lazyByteString "\r\n"
-                <> B.lazyByteString key
-                <> B.lazyByteString "\r\n"
-        in SocketContextInterpreter $ do
-            writeCommunicator (B.toLazyByteString stringToSend)
-            void $ readCommunicator 1
-            byteCount <- readUntil (== "\r")
-            void $ readCommunicator 1
-            let bytesToParse = read (unpack byteCount)
-            if bytesToParse == -1
-                then pure Nothing
-                else fmap Just (readCommunicator bytesToParse) <* readCommunicator 2
+    get key = SocketContextInterpreter $ do
+        writeCommunicator $ asRequest (get key)
+        void $ readCommunicator 1
+        byteCount <- readUntil (== "\r")
+        void $ readCommunicator 1
+        let bytesToParse = read (unpack byteCount)
+        if bytesToParse == -1
+            then pure Nothing
+            else fmap Just (readCommunicator bytesToParse) <* readCommunicator 2
         
     set = undefined
 
