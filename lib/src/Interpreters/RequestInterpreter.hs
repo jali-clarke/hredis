9524e706219676ba139595b01f76b4c4c26999a8
Coder <coder@dev.lan>
2019-09-08 15:29:57 +0000

    refactor get request, need to do set after

--- lib/src/Interpreters/RequestInterpreter.hs
+++ lib/src/Interpreters/RequestInterpreter.hs
@@ -15,17 +15,31 @@ import Redis
 
 newtype Serializing a = Serializing B.ByteString
 
+showableAsBytes :: Show a => a -> B.ByteString
+showableAsBytes = pack . show
+
 lengthAsBytes :: B.ByteString -> B.ByteString
-lengthAsBytes = pack . show . B.length
+lengthAsBytes = showableAsBytes . B.length
 
 builderWithReturn :: B.ByteString -> B.Builder
 builderWithReturn bytes = B.lazyByteString bytes <> B.lazyByteString "\r\n"
 
+requestString' :: B.ByteString -> (Int, B.Builder) -> (Int, B.Builder)
+requestString' bytes (numEntries, builder) =
+    let serializedEntry = B.lazyByteString "$"
+            <> builderWithReturn (lengthAsBytes bytes)
+            <> builderWithReturn bytes
+    in (numEntries + 1, serializedEntry <> builder)
+
+requestString :: [B.ByteString] -> Serializing a
+requestString bytesList =
+    let (numEntries, builder) = foldr requestString' (0, mempty) bytesList
+    in Serializing . B.toLazyByteString $ B.lazyByteString "*"
+        <> builderWithReturn (showableAsBytes numEntries)
+        <> builder
+
 instance Redis Serializing where
-    get key = Serializing . B.toLazyByteString $
-        B.lazyByteString "*2\r\n$3\r\nGET\r\n$"
-        <> builderWithReturn (lengthAsBytes key)
-        <> builderWithReturn key
+    get key = requestString ["GET", key]
 
     set key value = Serializing . B.toLazyByteString $
         B.lazyByteString "*3\r\n$3\r\nSET\r\n$"
